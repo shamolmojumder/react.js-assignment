efc8e93774724873467102fa02015c8952c73a56
shamolmojumder <shamolmojumder14@gmail.com>
2020-10-13 08:40:11 -0700

    13/10/20

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './components/data/data.json'
+import Body from './components/Body/Body';
 
 function App() {
+  console.log(data);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Body data={data}></Body>
+      <ul>
+        {
+          data.map(dat=> <li> {dat.fullname} </li> )
+        }
+      </ul>
     </div>
   );
 }
